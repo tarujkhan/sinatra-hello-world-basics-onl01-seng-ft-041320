6d6666f9c6e4fa59f64f34fd2f4ae27f9b856300
taroj khan <tarujkhan@yahoo.com>
2020-05-24 19:02:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-<<<<<<< HEAD
+#<<<<<<< HEAD
   get '/' do
     "Hello, World!"
   end
 end
-=======
+#=======
 
-"Hello, World!"
-end
-end 
->>>>>>> 8fbf2e4bcfda34a9a325cf3a2a23be9226abefc0
+#"Hello, World!"
+#end
+#end 
+#>>>>>>> 8fbf2e4bcfda34a9a325cf3a2a23be9226abefc0
