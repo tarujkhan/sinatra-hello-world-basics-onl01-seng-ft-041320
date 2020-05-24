ac17b9af93f1ef668cc2c27b628cd6f8a946468c
taroj khan <tarujkhan@yahoo.com>
2020-05-24 23:58:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
-class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-  get '/' do
-    "Hello, World!"
-  end
+class App < Sinatra::Base 
   
+  get '/' do 
+    "Hello, World!"
+  end 
 end
-
-
